d206722ef6284d46b2b7457f067b6190c576ce5c
Paul Hoffman <paul@flo.org>
2014-07-09 02:14:12 -0400

    Simplify constructor

--- lib/File/Feed/Context.pm
+++ lib/File/Feed/Context.pm
@@ -7,19 +7,8 @@ use String::Expando;
 
 sub new {
     my $cls = shift;
-    die if @_ % 2;
-    my %stash;
-    while (@_ >= 2) {
-        my ($key, $val) = splice @_, 0, 2;
-        if (ref($val) eq 'HASH') {
-            %stash = ( %stash, %$val, $key => $val );
-        }
-        else {
-            %stash = ( %stash, $key => $val );
-        }
-    }
     bless {
-        'stash' => \%stash,
+        'stash' => { @_ },
         'expando' => String::Expando->new,
     }, $cls;
 }
