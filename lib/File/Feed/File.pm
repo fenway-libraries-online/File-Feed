bec80898d773bbeb0c2e07b52d012efe06e547d7
Paul Hoffman <paul@flo.org>
2014-07-09 02:15:08 -0400

    Fix channel, feed, and source (but it's kludgy)

--- lib/File/Feed/File.pm
+++ lib/File/Feed/File.pm
@@ -9,12 +9,15 @@ sub new {
     my %arg = @_;
     $arg{'#'} ||= delete $arg{'id'};
     $arg{'@'} ||= 'file';
+    $arg{'channel'} = $arg{'_channel'}->id;
     bless \%arg, $cls;
 }
 
 sub id { $_[0]->{'#'} }
 sub path { $_[0]->{'#'} }
 sub local_path { $_[0]->{'local-path'} }
+sub feed { $_[0]->{'_feed'} }
+sub source { $_[0]->{'source'} }
 sub channel { $_[0]->{'_channel'} }
 
 1;
